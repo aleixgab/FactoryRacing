5d1b66167af0f17a489c7455817fb1f2fa006ddd
Óscar Hernández <oscarhernandezgomez@gmail.com>
2019-12-19 12:59:39 +0100

    Select car scene

--- Assets/RotateCamera.cs
+++ Assets/RotateCamera.cs
@@ -5,16 +5,11 @@ using UnityEngine;
 public class RotateCamera : MonoBehaviour
 {
     public GameObject target;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(target.transform.position, Vector3.up, 30 * Time.deltaTime);
+        transform.RotateAround(target.transform.position, Vector3.up, -15 * Time.deltaTime);
         transform.LookAt(target.transform.position);
     }
 }
